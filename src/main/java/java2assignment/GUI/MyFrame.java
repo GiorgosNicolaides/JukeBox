105f6771067423e29d02bf0e22f41b577925b291
Giorgos <gnicolaides02@gmail.com>
2022-11-18 15:26:17 +0200

    Gui

--- src/main/java/java2assignment/GUI/MyFrame.java
+++ src/main/java/java2assignment/GUI/MyFrame.java
@@ -5,30 +5,28 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame  extends JFrame {
+public class MyFrame extends JFrame {
     /**
      * 
      */
+
     
-    
-    public MyFrame(){
+
+    public MyFrame() {
+      
         
-         //Create the frame
-         this.setTitle("MusicPlayer");//set tittle
-         this.setSize(420 , 420);// set size 
-         this.setVisible(true); //make frame visible
-         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//exit out of application
-         this.getContentPane().setBackground(new Color(255 , 0, 0));//change background 
+
+        // Create the frame
+        this.setTitle("MusicPlayer");// set tittle
+        this.setSize(420, 420);// set size
+        this.setVisible(true); // make frame visible
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);// exit out of application
+        this.getContentPane().setBackground(new Color(255, 0, 0));// change background
+
+        // Create a button and add it to the frame
+
         
-        //Create a button and add it to the frame
-        
-        JButton play = new JButton();
-        play.setBounds(10 ,80 , 80 , 25);
-        this.add(play);
-       
 
     }
 
-
-    
 }
