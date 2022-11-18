a7d988a8fced6b6d31964473abaacd7594c4489f
Giorgos <gnicolaides02@gmail.com>
2022-11-18 18:45:00 +0200

    play button

---
+++ src/main/java/java2assignment/GUI/PlayButton.java
@@ -0,0 +1,16 @@
+package java2assignment.GUI;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class PlayButton extends JButton{
+    
+
+    public PlayButton(){
+        this.setBackground(new Color( 255 , 255 , 255) );
+        this.setBounds(50, 50, 30, 30);
+        
+
+    }
+}
