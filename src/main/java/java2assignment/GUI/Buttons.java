e73ac06025c5266b37a637839b8a392b9362703a
Giorgos <gnicolaides02@gmail.com>
2022-11-18 16:12:00 +0200

    Gui

---
+++ src/main/java/java2assignment/GUI/Buttons.java
@@ -0,0 +1,16 @@
+package java2assignment.GUI;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class Buttons extends JButton{
+    
+
+    public Buttons(){
+        this.setBackground(new Color( 255 , 255 , 255) );
+        this.setBounds(300, 200, 30, 30);
+        
+
+    }
+}
--- src/main/java/java2assignment/GUI/MyFrame.java
+++ src/main/java/java2assignment/GUI/MyFrame.java
@@ -2,14 +2,13 @@ package java2assignment.GUI;
 
 import java.awt.Color;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
     /**
      * 
      */
-
+    Buttons play = new Buttons();
     
 
     public MyFrame() {
@@ -18,12 +17,16 @@ public class MyFrame extends JFrame {
 
         // Create the frame
         this.setTitle("MusicPlayer");// set tittle
-        this.setSize(420, 420);// set size
+        // this.setSize(420, 420);// set size
+        this.setBounds(0    , 0, 400, 400);
+        this.setLayout(null);
         this.setVisible(true); // make frame visible
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);// exit out of application
-        this.getContentPane().setBackground(new Color(255, 0, 0));// change background
+        this.getContentPane().setBackground(new Color(0, 0, 0));// change background
+        
 
         // Create a button and add it to the frame
+        this.add(play);
 
         
 
