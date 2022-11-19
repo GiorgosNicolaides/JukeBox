8364f77e9c7f2a08042abeb6a42794fb64411f16
Giorgos <gnicolaides02@gmail.com>
2022-11-19 19:35:27 +0200

    Gui

--- src/main/java/java2assignment/GUI/MyFrame.java
+++ src/main/java/java2assignment/GUI/MyFrame.java
@@ -1,40 +1,17 @@
 package java2assignment.GUI;
 
-import java.awt.Color;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame extends JFrame {
-    /**
-     * 
-     */
-    // Creating Buttons
-    JButton play = new PlayButton();
-    JButton pause = new PauseButton();
-    JButton repeat = new RepeatButton();
-    JButton random = new RandomButton();
-    JButton next = new NextButton();
+public class MyFrame extends JFrame{
 
-    public MyFrame() {
+    public MyFrame(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBounds(0, 0, 1000, 1000);
+        this.setResizable(false);
+        this.setVisible(true);
+        
 
-        // Create the frame
-        this.setTitle("MusicPlayer");// set tittle
-        this.setBounds(0, 0, 400, 400); // set the bounds of the GUI
-
-        this.setLayout(null);
-        this.setResizable(false);//prevent user from resizing the window
-        this.setVisible(true); // make frame visible
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);// exit out of application
-        this.getContentPane().setBackground(new Color(0, 0, 0));// change background
-
-        // Add Buttons to the frame
-        this.add(play);
-        this.add(pause);
-        this.add(repeat);
-        this.add(random);
-        this.add(next);
 
     }
-
 }
