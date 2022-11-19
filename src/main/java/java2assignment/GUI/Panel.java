326da4f0248b8b9d2b8e01f2e8cbbebd802f4e96
Giorgos <gnicolaides02@gmail.com>
2022-11-19 21:10:47 +0200

    Panel

---
+++ src/main/java/java2assignment/GUI/Panel.java
@@ -0,0 +1,15 @@
+package java2assignment.GUI;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class Panel extends JPanel {
+
+    public Panel(){
+     // create the left panel
+     
+     this.setBackground(Color.darkGray);// Color
+     this.setBounds(0, 0, 300, 1000);// set Bounds
+    }
+    
+}
