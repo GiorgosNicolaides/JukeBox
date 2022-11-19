4f62401ba7998cd102dca7f8ce26320f677eb8c2
Giorgos <gnicolaides02@gmail.com>
2022-11-19 21:10:41 +0200

    Panel

--- src/main/java/java2assignment/GUI/MyFrame.java
+++ src/main/java/java2assignment/GUI/MyFrame.java
@@ -1,46 +1,37 @@
 package java2assignment.GUI;
-
-
 import java.awt.Color;
-
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame {
 
-    public MyFrame(){
+    public MyFrame() {
 
-        //Create the frame
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//set close button 
-        this.setBounds(0, 0, 1500, 1000);//set Bounds
-        this.setResizable(false);//not resizabke
-        this.setVisible(true);//visibility
+        // Create the frame
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);// set close button
+        this.setBounds(0, 0, 1500, 1000);// set Bounds
+        this.setResizable(false);// not resizabke
+        this.setVisible(true);// visibility
         this.setLayout(null);
-        this.getContentPane().setBackground(new Color(0x808080));//Color
+        this.getContentPane().setBackground(new Color(0x808080));// Color
         this.setTitle("MusicPlayer");
-        
-        
-        
 
-        //create the left panel
-        JPanel left = new JPanel();
-        left.setBackground(Color.darkGray);//Color
-        left.setBounds(0,0,300,1000);//set Bounds
-
-        //TODO
+        // get the left panel
+        Panel left  = new Panel();
+        // TODO
         // add pay button
         // add pause button
         // add repeat button
         // add random button
         // add next song button
 
+        // add playlist label and button
+        // add song playlist and button
 
-        //Add componenents
+
+        
+        // Add componenents
         this.add(left);
-
-
-
 
     }
 }
