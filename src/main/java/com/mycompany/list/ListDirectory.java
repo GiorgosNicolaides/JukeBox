ea26a7e411ae9f4a3251ce6292d924b5427758a8
Giorgos <gnicolaides02@gmail.com>
2022-12-08 17:46:08 +0200

    first commit

--- src/main/java/com/mycompany/list/ListDirectory.java
+++ src/main/java/com/mycompany/list/ListDirectory.java
@@ -12,24 +12,32 @@ import java.util.ArrayList;
  *
  * @author giorgos
  */
+
+//class for directory list
+
+
 public class ListDirectory extends ArrayList {
-    
+    //create variable to list the files
     File[] listOfFiles; 
     
-    
+    //get from args the directory and add the mp3 songs to an ArrayList
     public ListDirectory(File folder) throws FileNotFoundException{
         
         listOfFiles = folder.listFiles();
+        //using for to add the songs 
         for (File file : listOfFiles) {
+            //getting the filename
             String fileName = file.getName();
             if (fileName.endsWith(".mp3")) {
+                //adding to the list
                 this.add(file);
             }
         }
              
       } 
       
-    
+    //getter in order to get the song from other classe 
+    //will be used in stategy where we will get the list to implement random, order, loop
     File getFile( int number){
         return this.getFile(number);
     }
